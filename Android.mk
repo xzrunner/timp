8ac706a7fdc5edccc77827186534b3bc13033b50
zz <zhuguang@ejoy.com>
2017-10-23 16:11:35 +0800

    up makefile

--- Android.mk
+++ Android.mk
@@ -7,11 +7,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := timp
 
 LOCAL_C_INCLUDES  := \
+	${CLIB_PATH} \
 	${TIMP_SRC_PATH}/include/timp \
-	${CU_SRC_PATH} \
 	${FS_SRC_PATH} \
 	${BIMP_SRC_PATH}/include \
 	${LOGGER_SRC_PATH} \
+	${MEMMGR_SRC_PATH}/include \
 
 LOCAL_SRC_FILES := \
 	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.cpp" -print)) \
